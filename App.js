dddaef0f587542050b7d169c3fac665afe321579
milindrapid <66380150+milindrapid@users.noreply.github.com>
2020-06-05 12:43:36 +0530

    Grid/List View Toggle

---
+++ App.js
@@ -0,0 +1,114 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { dummyData } from "./data";
+
+import SkeletonCard from "./components/SkeletonCard";
+// const Tag =React.lazy(()=>import(./components/SkeletonCard))
+import Pagination from "./common/customPagination/pagination.component";
+import Search from "./components/search/Search";
+import ToolCard from "./components/card/Tools.components";
+import UserNameBar from "./components/userNameBar/UserNameBar.Component";
+import GridIcon from "./images/grid-icon.svg";
+import ListIcon from "./images/list-icon.svg";
+
+const App = () => {
+  const [videos, setVideos] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [gridView, setGridView] = useState(true);
+  const [listView, setListView] = useState(false);
+
+  // Load this effect on mount
+  useEffect(() => {
+    setLoading(true);
+    const timer = setTimeout(() => {
+      setVideos(dummyData);
+      setLoading(false);
+    }, 500);
+    // Cancel the timer while unmounting
+    return () => clearTimeout(timer);
+  }, []);
+
+  const onPageChanged = (data) => {
+    console.log("current page", data.currentPage);
+  };
+
+  const getSearchResult = (searchKey) => {
+    console.log("search", searchKey);
+  };
+
+  const handleList = () => {
+    setGridView(false);
+    setListView(true);
+  };
+
+  const handleGrid = () => {
+    setGridView(true);
+    setListView(false);
+  };
+
+  const onSearchMenuIconClick = (id) => {
+    switch (id) {
+      case 1:
+        return alert("Email");
+      case 2:
+        return alert("Calendar");
+      case 3:
+        return alert("Activity");
+      case 4:
+        return alert("Favorite Tools");
+      default:
+        break;
+    }
+  };
+
+  return (
+    <div className="App">
+      <Search
+        searchCallBack={getSearchResult}
+        onSearchMenuIconClick={onSearchMenuIconClick}
+      />
+      <UserNameBar userName="Taylor" />
+      <div>
+        <div
+          className="btn-group"
+          style={{ paddingTop: "15px", float: "right" }}
+        >
+          <img
+            className="search-menu-icon"
+            src={GridIcon}
+            onClick={() => handleGrid()}
+            alt="grid"
+          />
+          <img
+            className="search-menu-icon"
+            src={ListIcon}
+            onClick={() => handleList()}
+            alt="list"
+          />
+        </div>
+      </div>
+
+      {loading && <SkeletonCard />}
+      {!loading &&
+        videos.map((list, index) => {
+          return (
+            <section key={index}>
+              <h2 className="section-title">{list.section}</h2>
+              <ToolCard listView={listView} gridView={gridView} />
+              <hr />
+            </section>
+          );
+        })}
+
+      <Pagination
+        currentPage={2}
+        totalCount={26}
+        itemsPerPage={3}
+        pageRange={4}
+        onChange={onPageChanged}
+      />
+    </div>
+  );
+};
+
+export default App;
---
+++ Tools.components.jsx
@@ -0,0 +1,101 @@
+import React, { useState } from "react";
+import classNames from "classnames";
+import ToolCards from "./ToolCard";
+import { TOOL_DATA } from "../../data";
+import { FAVORITE_TOOL } from "../../data";
+import GridIcon from "../../images/grid-icon.svg";
+import ListIcon from "../../images/list-icon.svg";
+
+const Tools = (props) => {
+  console.log(props);
+  const [id, setId] = useState("");
+  const [gridView, setGridView] = useState(true);
+  const [favoriteTool, setFavoriteTool] = useState(0);
+
+  const onRadioChange = (id) => {
+    setFavoriteTool(id);
+  };
+
+  const btnClass = classNames("item", {
+    "base-container-list": props.listView,
+    "base-container-grid": props.gridView,
+  });
+
+  const getFavoriteTool = () => {
+    return FAVORITE_TOOL.map((tool) => (
+      <div className="radio">
+        <input
+          className="radio-button"
+          checked={tool.id === favoriteTool}
+          type="radio"
+          value={tool.value}
+          onChange={() => onRadioChange(tool.id)}
+        />
+        {tool.value}
+      </div>
+    ));
+  };
+
+  const getToolItem = () => {
+    let toolsList =
+      id === "" ? TOOL_DATA : TOOL_DATA.filter((item) => item.id === id);
+    return toolsList.length === 0 ? (
+      <label style={{ margin: "50px" }}>No Tool Avalibale</label>
+    ) : (
+      toolsList.map((tool) => (
+        <div>
+          <ToolCards
+            id={tool.id}
+            title={tool.title}
+            content={tool.content}
+            imageUrl={tool.imageUrl}
+            isFavorite={tool.isFavorite}
+            favIconClickHandle={() => favIconClickHandle(tool.id)}
+          />
+        </div>
+      ))
+    );
+  };
+
+  const favIconClickHandle = (id) => {
+    console.log("favId", id);
+  };
+
+  const searchCallBack = (event) => {
+    if (event.which === 13) {
+      //code of enter button is 13
+      let searchId = event.target.value;
+      if (!isNaN(searchId)) {
+        setId(searchId);
+      } else {
+        alert("Invalid Id");
+      }
+    }
+  };
+
+  const showViewChangeIcon = () => {
+    let icon = "";
+    icon = gridView ? ListIcon : GridIcon;
+    return (
+      <img
+        className="view-change-icon "
+        src={icon}
+        alt={icon}
+        onClick={changeView}
+      />
+    );
+  };
+
+  const changeView = () => {
+    setGridView(gridView ? false : true);
+  };
+
+  return (
+    <div>
+      {/* {showViewChangeIcon()} */}
+      <div className={btnClass}>{getToolItem()}</div>
+    </div>
+  );
+};
+
+export default Tools;
