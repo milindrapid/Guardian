e43338da9e5c98544b600f1f536c36703f2cf036
Sayana C S <sayana.cs@rapidvaluesolutions.com>
2020-06-10 15:55:21 +0530

    Event card

---
+++ src/components/EventCard/EventCard.Component.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './EventCard.scss';
+
+const EventCard = (props) => {
+    return (
+        <div className="event-card-container">
+            <img className="banner" src={require('../../images/sample-image.png')} alt="sample" />
+            <div className="data-view">
+                <div className="event-details">
+                    <span className="event-title" onClick={() => props.onItemClick()}>{props.eventDetails.event}</span>
+                    <span className="event-sche">{props.eventDetails.date}|{props.eventDetails.time}</span>
+                </div>
+                
+                {/* <div className="event-date">
+                    <span className="event-sche">{props.eventDetails.date}</span>
+                    <span className="event-sche">|</span>
+                    <span className="event-sche">{props.eventDetails.time}</span>
+                </div> */}
+                <div className="event-more">
+                    <a className="learn-more-link" href="/" >Learn more</a>
+                    <label className="streaming"><span className="ic-streaming"></span>STREAMING</label>
+                </div>
+            </div>
+        </div >
+    );
+}
+
+export default EventCard;
