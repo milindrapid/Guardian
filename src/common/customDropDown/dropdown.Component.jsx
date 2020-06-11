e503a521bd3a8f761d16089f0ef3bac6ce3d88a9
Devesh Chauhan <devesh.chauhan@rapidvaluesolutions.com>
2020-06-11 16:25:57 +0530

    Revert auto formatting

--- src/common/customDropDown/dropdown.Component.jsx
+++ src/common/customDropDown/dropdown.Component.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-import "./dropdown.scss";
+import React, { useState, useEffect } from 'react';
+import './dropdown.scss';
 
 /**
  * @description
@@ -10,60 +10,49 @@ import "./dropdown.scss";
  * @param {string} optionText Field of the object to be displayed - Required eg: item
  * @param {string} optionValue Field of the object to be set as value - Required
  * @param {Function} onItemChange Callback function to be invoked when changes page
- *
+ * 
  * @returns {item} currentPage - Currently selected item
  */
 
 const CustomDropDown = (props) => {
-  const [selectedOption, setSelectedOption] = useState(null);
-  const [displayLabel, setDisplayLabel] = useState("");
+    const [selectedOption, setSelectedOption] = useState(null);
+    const [displayLabel, setDisplayLabel] = useState('');
 
-  useEffect(() => {
-    let tempSelected = props.selectedOption ? props.selectedOption : null;
-    if (!tempSelected && props.items && props.items.length) {
-      tempSelected = props.items[0];
-      if (props.optionValue) {
-        tempSelected = tempSelected[props.optionValue];
-      }
+    useEffect(() => {
+        let tempSelected = props.selectedOption ? props.selectedOption : null;
+        if (!tempSelected && props.items && props.items.length) {
+            tempSelected = props.items[0];
+            if (props.optionValue) {
+                tempSelected = tempSelected[props.optionValue];
+            }
+        }
+        setSelectedOption(tempSelected);
+        setDisplayLabel(props.label ? props.label : 'Select');
+    }, [props.items, props.label, props.optionValue, props.selectedOption]);
+
+    const onItemChange = (event) => {
+        setSelectedOption(event.target.value);
+        props.onItemChange({ selected: event.target.value });
     }
-    setSelectedOption(tempSelected);
-    setDisplayLabel(props.label ? props.label : "Select");
-  }, [props.items, props.label, props.optionValue, props.selectedOption]);
 
-  const onItemChange = (event) => {
-    setSelectedOption(event.target.value);
-    props.onItemChange({ selected: event.target.value });
-  };
-
-  return (
-    <>
-      <div className="custom-dropdown">
-        <select
-          className="custom-dropdown-select"
-          style={props.selectClass ? props.selectClass : null}
-          id="customDropdown"
-          value={selectedOption}
-          onChange={(event) => onItemChange(event)}
-        >
-          <option
-            style={props.optionClass ? props.optionClass : null}
-            selected={!selectedOption ? true : false}
-          >
-            {displayLabel}
-          </option>
-          {props.items.map((item) => {
-            return (
-              <option
-                value={props.optionValue ? item[props.optionValue] : item}
-              >
-                {item[props.optionText]}
-              </option>
-            );
-          })}
-        </select>
-      </div>
-    </>
-  );
-};
+    return (
+        <>
+            <div className="custom-dropdown">
+                <select className="custom-dropdown-select" style={props.selectClass ? props.selectClass : null} id="customDropdown" value={selectedOption} onChange={(event) => onItemChange(event)}>
+                    <option style={props.optionClass ? props.optionClass : null} selected={!selectedOption ? true : false}>{displayLabel}</option>
+                    {
+                        props.items.map((item) => {
+                            return (
+                                <option value={props.optionValue ? item[props.optionValue] : item}>
+                                    {item[props.optionText]}
+                                </option>
+                            );
+                        })
+                    }
+                </select>
+            </div>
+        </>
+    )
+}
 
 export default CustomDropDown;
