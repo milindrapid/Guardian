009a0fb3cce9ec7746e6045120c0ecb2c0ea087b
Milind Gunjan <milind.gunjan@ADRAPIDVALUE.COM>
2020-06-12 13:25:28 +0530

    Toggle Button added in Tools

--- src/pages/Tools/Tools.js
+++ src/pages/Tools/Tools.js
@@ -1,43 +1,86 @@
-import React from "react";
+import React, { useState } from "react";
 import ToolCard from "../../components/Tools/ToolCard";
 import ToolList from "../../components/Tools/ToolList";
 import "./Tools.scss";
 import toolsData from "../../mockData/toolsData";
+import GridIcon from "../../images/grid.svg";
+import ListIcon from "../../images/list-icon.svg";
 
 const Tools = () => {
-  return (
-    <div className="event-container">
-      <h2 className="main-title">Card View</h2>
-      <div className="event-data-container">
-        {toolsData.map((tools, index) => {
-          return (
-            <div className="col" key={index}>
-              <ToolCard
+  const [gridView, setGridView] = useState(true);
+  const [listView, setListView] = useState(false);
+
+  const handleList = () => {
+    setGridView(false);
+    setListView(true);
+  };
+
+  const handleGrid = () => {
+    setGridView(true);
+    setListView(false);
+  };
+  console.log(gridView, listView);
+
+  const getToolItem = () => {
+    if (gridView) {
+      console.log("Yes");
+      return (
+        <div className="event-data-container">
+          {toolsData.map((tools, index) => {
+            return (
+              <div className="col" key={index}>
+                <ToolCard
+                  isFavorite={tools.isFavorite}
+                  title={tools.title}
+                  description={tools.description}
+                  callToAction={tools.callToAction}
+                  callToMore={tools.callToMore}
+                />
+              </div>
+            );
+          })}
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          {toolsData.map((tools, index) => {
+            return (
+              <ToolList
+                key={index}
                 isFavorite={tools.isFavorite}
                 title={tools.title}
                 description={tools.description}
                 callToAction={tools.callToAction}
                 callToMore={tools.callToMore}
               />
-            </div>
-          );
-        })}
-      </div>
-      <h2 className="main-title">List View</h2>
+            );
+          })}
+        </div>
+      );
+    }
+  };
+
+  return (
+    <div className="event-container">
       <div>
-        {toolsData.map((tools, index) => {
-          return (
-            <ToolList
-              key={index}
-              isFavorite={tools.isFavorite}
-              title={tools.title}
-              description={tools.description}
-              callToAction={tools.callToAction}
-              callToMore={tools.callToMore}
-            />
-          );
-        })}
+        <h2 className="main-title">All Tools</h2>
+        <div style={{ float: "right", paddingRight: "30px" }}>
+          <img
+            className="event-menu-icon"
+            src={GridIcon}
+            onClick={() => handleGrid()}
+            alt="grid"
+          />
+          <img
+            className="event-menu-icon"
+            src={ListIcon}
+            onClick={() => handleList()}
+            alt="list"
+          />
+        </div>
       </div>
+      <div className="event-data-container">{getToolItem()}</div>
     </div>
   );
 };
