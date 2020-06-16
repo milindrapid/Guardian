c0026bde3058ae7ff3bf3881f988dfd30b472354
adarsh_s <adarsh.s@rapidvaluesolutions.com>
2020-06-16 12:38:04 +0530

    Add common searchbar

---
+++ src/common/customSearch/Search.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Search.scss';
+import SearchIcon from '../../../Images/search-icon.svg';
+
+const searchBar = (props) => {
+
+    const searchResultCallBack = (event) => {
+        if (event.which === 13) {
+            props.searchResultCallBack(event.target.value)
+        }
+    }
+
+    return (
+        <div style={props.searchBarWidth} className="search-container">
+            <img className="search-icon" src={SearchIcon} alt="search" />
+            <input
+                className="search-input"
+                type="text"
+                placeholder={props.placeHolder}
+                onKeyPress={searchResultCallBack} />
+        </div>
+    )
+}
+
+export default searchBar;
