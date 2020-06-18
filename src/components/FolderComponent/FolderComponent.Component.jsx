8c62bb3faa2177e8cce54680d7e13b23aaf9c241
Sayana C S <sayana.cs@rapidvaluesolutions.com>
2020-06-18 14:44:49 +0530

    Styled components - Folder component, RecentData

--- src/components/FolderComponent/FolderComponent.Component.jsx
+++ src/components/FolderComponent/FolderComponent.Component.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
-import './FolderComponent.scss';
+import styled from 'styled-components';
+
+const FolderItem = styled.div`
+    padding: 1.5rem 3.5rem;
+    background-color: #E9E9E7;
+    text-align: center;
+    margin: 0.5rem 0;
+`;
+const FolderName = styled.p`
+    margin-bottom: 0;
+    font-weight: 600;
+    font-size: 1rem;
+    text-decoration: underline;
+`;
 
 const FolderComponent = (props) => {
     return (
-        <div className="folder-item">
-            <p className="folder-name">{props.folderDetails.name}</p>
-            <p className="folder-content">{props.folderDetails.count} resources</p>
-        </div>
+        <FolderItem>
+            <FolderName>{props.folderDetails.name}</FolderName>
+            <p>{props.folderDetails.count} resources</p>
+        </FolderItem>
     );
 }
 
--- src/components/RecentData/RecentData.Component.jsx
+++ src/components/RecentData/RecentData.Component.jsx
@@ -1,19 +1,48 @@
 import React from 'react';
+import styled from 'styled-components';
 import './RecentData.scss';
+
+const RecentDataContainer = styled.div`
+    width: 20rem;
+    padding: 1rem;
+    border: 1px solid;
+    flex: 1;
+    margin: 0.5rem;
+    h5 {
+        font-weight: 700;
+        margin-bottom: 1rem;
+    }
+`;
+const RecentDataItem = styled.div`
+    padding: 1rem 0 1.5rem 0;
+    border-bottom: 1px solid #a3a39f;
+    p {
+        font-size: 0.9rem;
+        margin-bottom: 0;
+    }
+    p.recent-item-name {
+        font-weight: 700;
+        text-decoration: underline;
+    }
+    &:last-child {  
+        padding-bottom: 0;
+        border-bottom: none;
+    }
+`;
 
 const RecentData = (props) => {
     return (
-        <div className="recent-data">
+        <RecentDataContainer>
             <h5>{props.title}</h5>
             {props.itemList.map((item) => {
                 return (
-                    <div key={item.itemId} className="recent-data-item">
+                    <RecentDataItem key={item.itemId}>
                         <p className="recent-item-name">{item.itemTitle}</p>
                         <p>{item.itemDescription}</p>
-                    </div>
+                    </RecentDataItem>
                 );
             })}
-        </div>
+        </RecentDataContainer>
     );
 }
 
